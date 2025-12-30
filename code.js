9d10c4ef3289633042ae833f3a4cde5a2476c12f
Szymon <tabolator1@gmail.com>
2025-12-30 20:45:45 +0100

    lepiej wygląda i jest HP

--- code.js
+++ code.js
@@ -1,10 +1,15 @@
 function showPlayers(data){
-    
+    let img ="ta_taka_stojakczykichuj.png"
+    if(data.lives == 2){
+        img = "2hp.png"
+    }else if(data.lives == 1){ img = "1hp.png"}
+    else if(data.lives == 3){ img = "ta_taka_stojakczykichuj.png"}
+    else{ img = "0hp.png"}
     const player = `
         <div class="player" style="transform: rotateY(${kurwiszon[0]}deg) rotate(${kurwiszon[3]}deg) scale(${kurwiszon[4]}) ; left:${kurwiszon[1]}px; top:${kurwiszon[2]}px">
-            <img src="./img/ta_taka_stojakczykichuj.png" class="stojak"/>
+            <img src="./img/${img}" class="stojak"/>
             <img src="http://83.27.64.116:3000/upload/${data.name}.png" class="playerImage"/>
-            <p class="playerName">${data.name}</p>
+            <p class="playerName">${data.id+1}. ${data.name}</p>
         </div>
     `
     if(data.button == true){
@@ -32,7 +37,7 @@ function przycisk(data){
     sigmaa.style.backgroundPosition = '0px 0px'
     sigmaa6.style.backgroundPosition = '0px 200px'
         setTimeout(() => {
-        sigmaa.style.backgroundSize='50%'
+        sigmaa.style.backgroundSize='30%'
     }, "100");
 
     setTimeout(() => {
@@ -41,12 +46,13 @@ function przycisk(data){
     sigmaa3.style.display="none"
     sigmaa4.style.display="none"
     sigmaa6.style.display="none"
-    }, "2000");
+    }, "2500");
 
 }
 let playerList = []
 let playeramount = 0
 let serokosc = 0
+let ololo = 0
 let kurwiszon = [-70,0,250,-20,3]
 async function getPlayersLong(){
     let response = await fetch("http://83.27.64.116:3000/playerList")
@@ -62,12 +68,13 @@ async function getPlayersLong(){
     else{
         let message = await response.text()
         if (playerList!=message){
-            kurwiszon = [-70,0,serokosc/2,-40,4]
+            ololo =0
+            kurwiszon = [-70,0,serokosc/2,-30,2]
             playerList=message
             playerListNormalised = JSON.parse(playerList)
             players.innerHTML=""
             playeramount = playerListNormalised.length-1
-            serokosc = players.offsetWidth -500
+            serokosc = players.offsetWidth -300
             gura = 0
             
             playerListNormalised.forEach(player=>{
@@ -75,22 +82,29 @@ async function getPlayersLong(){
                 if(promien-kurwiszon[1]>0){zabijesie = promien-kurwiszon[1]}
                 else{zabijesie = kurwiszon[1]-promien}
                 if(kurwiszon[0]+90<=90 && zabijesie!=0 ){
-                kurwiszon[2] = serokosc/2 -Math.tan(((kurwiszon[0]+90)*Math.PI)/180)*zabijesie
-                kurwiszon[4] -= 3/(playeramount/2)}
+                    kurwiszon[2] = serokosc/2 -Math.tan(((kurwiszon[0]+90)*Math.PI)/180)*zabijesie
+                    if(ololo == 0 || ololo == 1){ololo +=2}
+                    else{
+                        if(kurwiszon[4] - 1/((playeramount+1)/2) > 1){kurwiszon[4] -= 2/((playeramount+1)/2)}
+                    }
+                }
                 else if((-2.0>kurwiszon[0]&&kurwiszon[0]<2.0)|| kurwiszon[0]==0){
                     console.log("what the helly")
-                    kurwiszon[2] = 190
+                    kurwiszon[2] = 220
+                    ololo = 0
                     kurwiszon[4] = 1
                 }
                 else{
                    kurwiszon[2] = serokosc/2 -Math.tan(((90-kurwiszon[0])*Math.PI)/180)*zabijesie 
-                   kurwiszon[4] += 3/(playeramount/2)
+                   if(ololo == 0){ololo = 1}
+                    else{
+                   kurwiszon[4] += 1/((playeramount+1)/2)}
                 }
                 console.log(promien,zabijesie,kurwiszon[0],kurwiszon[2],kurwiszon[4])
                 players.innerHTML+=showPlayers(player)
                 kurwiszon[0]+=140/playeramount
                 kurwiszon[1]+=serokosc/(playeramount)
-                kurwiszon[3]+=80/playeramount
+                kurwiszon[3]+=60/playeramount
                 
                 console.log(player)
             })
@@ -99,3 +113,14 @@ async function getPlayersLong(){
     }
 }
 getPlayersLong()
+
+async function qrcodeGetter(){
+    let response = await fetch("http://83.27.64.116:3000/generateQRCode")
+    let message = await response.text()
+    qrcode.src=message
+}
+qrcodeGetter()
+
+function deleteQR(){
+    qrcode.style.display = "none"
+}
